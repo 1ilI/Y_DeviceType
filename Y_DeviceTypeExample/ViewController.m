15361f6470edadf40bf39bdfdd696fb6c8b19658
Yue <369581379@qq.com>
2018-06-26 10:11:50 +0800

    modify

--- Y_DeviceTypeExample/ViewController.m
+++ Y_DeviceTypeExample/ViewController.m
@@ -7,7 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "UIDevice+DeviceType.h"
 
+static NSString *const tableViewCellID = @"tableViewCellReuseIdentifier";
 @interface ViewController ()
 
 @end
@@ -17,8 +19,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:tableViewCellID];
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 10;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:tableViewCellID forIndexPath:indexPath];
+    if (indexPath.row == 3) {
+        cell.textLabel.text = [NSString stringWithFormat:@"设备类型：%@",[UIDevice deviceTypeName]];
+    }
+    else if (indexPath.row == 4) {
+        cell.textLabel.text = [NSString stringWithFormat:@"应用名称：%@",[UIDevice appDisplayName]];
+    }
+    else if (indexPath.row == 5) {
+        cell.textLabel.text = [NSString stringWithFormat:@"项目中的 Version：%@",[UIDevice appShortVersion]];
+    }
+    else if (indexPath.row == 6) {
+        cell.textLabel.text = [NSString stringWithFormat:@"项目中的 Build：%@",[UIDevice appBuildVersion]];
+    }
+    return cell;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
